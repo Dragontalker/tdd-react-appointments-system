c7d6ab4ff8f34339d4b3fc74581bb523b682ec18
Richard <richard.yang.tong@gmail.com>
2021-04-08 16:14:19 -0400

    Added Props:customer.

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,5 +1,6 @@
 describe('Appointment', () => {
     it('renders the customer first name', () => {
+        const customer = { firstName: 'Ashley' };
         expect(document.body.textContent).toMatch('Ashley');
     });
 });
