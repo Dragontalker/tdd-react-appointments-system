5683f861359a27769c9749ef10fe74cd5e753854
Richard <richard.yang.tong@gmail.com>
2021-04-08 16:15:28 -0400

    Created a new DOM element.

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -2,6 +2,7 @@ describe('Appointment', () => {
     it('renders the customer first name', () => {
         const customer = { firstName: 'Ashley' };
         const component = <Apppointment customer={customer} />;
+        const container = document.createElemenmt('div');
         expect(document.body.textContent).toMatch('Ashley');
     });
 });
