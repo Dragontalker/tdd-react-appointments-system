82c4bdca7ae5f69ce5571bbcade4b5c9d6ff3c31
Richard <richard.yang.tong@gmail.com>
2021-04-08 16:14:58 -0400

    Added component Appointment.

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,6 +1,7 @@
 describe('Appointment', () => {
     it('renders the customer first name', () => {
         const customer = { firstName: 'Ashley' };
+        const component = <Apppointment customer={customer} />;
         expect(document.body.textContent).toMatch('Ashley');
     });
 });
